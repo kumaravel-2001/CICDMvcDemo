71329c9cdc0a875aa97e5500d21bd753a53954eb
kumaravel <kumaravel23@gmail.com>
2022-05-31 17:45:34 +0530

    Commited

---
+++ CICDMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CICDMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
